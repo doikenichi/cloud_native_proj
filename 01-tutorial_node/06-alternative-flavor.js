e3a9bbf7b766dd717aafcf244c6dfc7101573fcf
K D <doi.san@gmail.com>
2026-05-15 15:59:09 +0000

    renamed directories

---
+++ 01-tutorial_node/06-alternative-flavor.js
@@ -0,0 +1,10 @@
+module.exports.items = ['item1', 'item2'];
+const person = {
+    name: 'bob',
+}
+
+module.exports.singlePerson = person;
+
+// same as module.exports = { items: ['item1', 'item2'], singlePerson: person }
+
+// console.log(module.exports);
--- tutorial/07-mind-grenade.js
+++
@@ -1,9 +0,0 @@
-const num1 =  5;
-const num2 = 10;
-
-function addValues() {
-    console.log(`The sum is: ${num1 + num2}`);
-}
-
-// functions are executed once the file is loaded in memory, so the function is executed on require.
-addValues();
