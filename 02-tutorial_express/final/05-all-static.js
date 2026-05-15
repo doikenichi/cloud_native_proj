4a2f0834354bfc0ac7358c3c81d0b54032c57439
K D <doi.san@gmail.com>
2026-05-15 20:07:18 +0000

    express part 1

---
+++ 02-tutorial_express/final/05-all-static.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// setup static and middleware
+// 1. create a new directory called public
+// 2. copy all files from navbar-app to public because index.html is also static asset
+app.use(express.static('./public'));
+
+// app.get('/', (req,res) => { 
+//     res.status(200).sendFile(path.resolve(__dirname, './navbar-app/index.html'));
+//   adding to static assets
+//   SSR
+// })
+
+app.all('/{*path}', (req,res)=>{
+    res.status(404).send('<h1>resource not found</h1>');
+})
+
+app.listen(5000, () => {
+    console.log('Server is listening on port 5000...');
+})
